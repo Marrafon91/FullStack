920b403df98a44246bcb622e51bfb3d58c30474e
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-05-01 16:22:18 -0300

    Seleção de elementos

---
+++ curso-nivelamento-JS/selecao/script.js
@@ -0,0 +1,25 @@
+const item1 = document.querySelector("li");
+
+console.log(item1.innerHTML);
+console.log(item1.outerHTML);
+
+const items = document.querySelectorAll("li");
+
+console.log(items);
+
+const card1 = document.querySelector(".card");
+console.log(card1);
+
+const paragrafo = document.createElement("p");
+paragrafo.innerHTML = "Descrição"
+card1.appendChild(paragrafo);
+
+card1.classList.add("super-border")
+
+const cards = document.querySelectorAll(".card")
+
+Array.from(cards).map(x => {
+    console.log(x.innerHTML);
+});
+
+console.log(cards);
