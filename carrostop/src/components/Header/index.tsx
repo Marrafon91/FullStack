950f99d6ec7b2fe7e6bb6222eea34af970936413
Guilherme <guilherme.marrafon1@gmail.com>
2026-05-13 11:58:48 -0300

    Carros Top componente Header

--- carrostop/src/components/Header/index.tsx
+++ carrostop/src/components/Header/index.tsx
@@ -3,8 +3,8 @@ import './style.css';
 export default function HeaderCar() {
   return (
     <div>
-      <header className="header-container">
-        <div className="title-container">
+      <header className="ct-header">
+        <div className="ct-container">
           <h1>Carros Top</h1>
         </div>
       </header>
