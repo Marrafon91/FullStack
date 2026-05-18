3b826ec4cc92ebf539e1903e693dba02a671e07e
Guilherme <guilherme.marrafon1@gmail.com>
2026-05-18 20:26:51 -0300

    Rota para não encontrado

--- tutorial-react-router/src/main.tsx
+++ tutorial-react-router/src/main.tsx
@@ -3,13 +3,15 @@ import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Expenses from './routes/Expenses/index.tsx';
 import Invoices from './routes/Invoices/index.tsx';
+import NotFound from './routes/NotFound/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="Invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   </BrowserRouter>,
