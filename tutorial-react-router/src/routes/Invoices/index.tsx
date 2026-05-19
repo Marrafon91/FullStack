ce82bb05ac977a53e427d11bfbb8c2d20ecfa61d
Guilherme <guilherme.marrafon1@gmail.com>
2026-05-19 08:17:17 -0300

    Filtrando barra de navegação com parâmetros de busca

--- tutorial-react-router/src/routes/Invoices/index.tsx
+++ tutorial-react-router/src/routes/Invoices/index.tsx
@@ -1,9 +1,11 @@
 import './style.css';
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
 import { getInvoices } from '../../data';
 
 export default function Invoices() {
   const invoices = getInvoices();
+
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <div style={{ display: 'flex' }}>
@@ -13,15 +15,36 @@ export default function Invoices() {
           padding: '1rem',
         }}
       >
-        {invoices.map((invoice) => (
-          <NavLink
-            className={({ isActive }) => (isActive ? 'dblock nav-red' : 'dblock nav-blue')}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          value={searchParams.get('name') || ''}
+          onChange={(e) => {
+            const name = e.target.value;
+            if (name) {
+              setSearchParams({ name });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+
+        {invoices
+          .filter((invoice) => {
+            const name = searchParams.get('name');
+            if (!name) return true;
+            const invoiceName = invoice.name.toLowerCase();
+            return invoiceName.includes(name.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? 'dblock nav-red' : 'dblock nav-blue'
+              }
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
       <Outlet />
     </div>
