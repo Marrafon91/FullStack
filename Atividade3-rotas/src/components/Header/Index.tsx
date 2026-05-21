83072d4ad75d26e81e11ec87e19b0ee146efbecb
Guilherme <guilherme.marrafon1@gmail.com>
2026-05-21 12:07:48 -0300

    Exercício 3

--- Atividade3-rotas/src/components/Header/Index.tsx
+++ Atividade3-rotas/src/components/Header/Index.tsx
@@ -1,13 +1,43 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './style.css';
 
 export default function Header() {
   return (
     <header className="main-header">
       <div className="container">
-        <Link to="/">
-          <h1>MeuSite</h1>
-        </Link>
+        <nav className="nav-list">
+          <div>
+            <Link to="/">
+              <h1>MeuSite</h1>
+            </Link>
+          </div>
+          <ul className="nav-items">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? 'menu-active' : '')}
+              >
+                Início
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/promotion"
+                className={({ isActive }) => (isActive ? 'menu-active' : '')}
+              >
+                Promoções
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/subscription"
+                className={({ isActive }) => (isActive ? 'menu-active' : '')}
+              >
+                Inscrição
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
