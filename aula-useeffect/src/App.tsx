30a36461d45f1211233fe2b8c9f4581d992fb98a
Guilherme <guilherme.marrafon1@gmail.com>
2026-05-23 09:47:15 -0300

    Hook useEffect PARTE 2

--- aula-useeffect/src/App.tsx
+++ aula-useeffect/src/App.tsx
@@ -2,29 +2,26 @@ import { useEffect, useState } from 'react';
 import { findProduct } from './data';
 
 export default function App() {
-  console.log("Passou no APP")
+  console.log('Passou no APP');
   const [productId, setProductId] = useState(1);
   const [prod, setProd] = useState<any>();
 
   useEffect(() => {
-    console.log("Passou no UseEffect")
+    console.log('Passou no UseEffect');
     setProd(findProduct(productId));
   }, [productId]);
 
   function handleClick() {
-    console.log("Passou no handleClick")
+    console.log('Passou no handleClick');
+    setProductId(2);
   }
 
   return (
     <>
       <h2>{prod?.name}</h2>
       <p>{prod?.price}</p>
-
       <br />
-
-      <button onClick={handleClick}>
-        Mudar para Produto 2
-      </button>
+      <button onClick={handleClick}>Mudar para Produto 2</button>
     </>
   );
 }
