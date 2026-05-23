8211ebf8cd52c7b89429748cc64f286fa0a7873f
Guilherme <guilherme.marrafon1@gmail.com>
2026-05-23 14:16:03 -0300

    Requisições para o catálogo e por id

--- dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -14,12 +14,12 @@ export default function ProductDetails() {
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
-    axios.get('http://localhost:8080/products/1').then((response) => {
-      console.log(response.data);
-      setProduct(response.data);
-    });
-
-
+    axios
+      .get(`http://localhost:8080/products/${params.productId}`)
+      .then((response) => {
+        console.log(response.data);
+        setProduct(response.data);
+      });
   }, []);
 
   return (
