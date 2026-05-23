605399e6c234c5231a32ac83f9a4e36bdee43217
Guilherme <guilherme.marrafon1@gmail.com>
2026-05-23 14:02:16 -0300

    Primeiro teste de requisição com Axios

---
+++ dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -0,0 +1,38 @@
+import './style.css';
+import ButtonInverse from '../../../components/ButtonInverse';
+import ButtonPrimary from '../../../components/ButtonPrimary';
+import ProductDetailsCard from '../../../components/ProductDetailsCard';
+import * as productService from '../../../services/product-service';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import type { ProductDTO } from '../../../models/product';
+import axios from 'axios';
+
+export default function ProductDetails() {
+  const params = useParams();
+
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/products/1').then((response) => {
+      console.log(response.data);
+      setProduct(response.data);
+    });
+
+
+  }, []);
+
+  return (
+    <main>
+      <section id="product-details-section" className="dsc-container">
+        {product && <ProductDetailsCard product={product} />}
+        <div className="dsc-btn-page-container">
+          <ButtonPrimary text="Comprar" />
+          <Link to="/">
+            <ButtonInverse text="Inicio" />
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+}
