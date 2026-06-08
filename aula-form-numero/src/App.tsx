476cb60709524ac5a7e8c2eae583d2e13f164681
Guilherme <guilherme.marrafon1@gmail.com>
2026-06-08 10:26:20 -0300

    Dica - campo numérico não preenchido em formulário

---
+++ aula-form-numero/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+type FormData = {
+  salary?: number;
+};
+
+export default function App() {
+  const [formData, setFormData] = useState<FormData>({});
+
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    console.log(formData.salary || Number.MAX_VALUE);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="salary"
+        value={formData.salary || ""}
+        type="text"
+        placeholder="Digite um valor maximo"
+        onChange={handleInputChange}
+      />
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
