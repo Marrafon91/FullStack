e752ff466a91378c0877585330e820ff9d9bd0e5
Guilherme <guilherme.marrafon1@gmail.com>
2026-06-11 20:31:36 -0300

    Layout da página Admin

---
+++ dscommerce-frontend-login/src/routes/Admin/AdminHome/index.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+export default function AdminHome() {
+  return (
+    <main>
+      <section id="admin-home-section" className="dsc-container">
+        <h2 className="dsc-section-title dsc-mb20">
+          Bem-vindo a área administrativa
+        </h2>
+      </section>
+    </main>
+  );
+}
