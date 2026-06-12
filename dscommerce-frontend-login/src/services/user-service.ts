0cf27eee77f4df5b1837a9868ff7fa339a86a8b9
Guilherme <guilherme.marrafon1@gmail.com>
2026-06-12 09:04:34 -0300

    Usando o atributo withCredentials do AxiosRequestConfig

--- dscommerce-frontend-login/src/services/user-service.ts
+++ dscommerce-frontend-login/src/services/user-service.ts
@@ -1,11 +1,11 @@
+import type { AxiosRequestConfig } from 'axios';
 import { requestBackend } from '../utils/request';
-import * as authService from './auth-service';
 
 export function findMe() {
-  const headers = {
-    Authorization: `Bearer ${authService.getAccessToken()}`,
+  const config: AxiosRequestConfig = {
+    url: '/users/me',
+    withCredentials: true,
   };
 
-  console.log(headers);
-  return requestBackend({ url: '/users/me', headers });
+  return requestBackend(config);
 }
