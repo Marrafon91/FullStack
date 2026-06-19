da2adb6a10cb1aaf4cd7b0ceed3ff6cc5c3f3864
Guilherme <guilherme.marrafon1@gmail.com>
2026-06-19 19:17:46 -0300

    Preparando as rotas da área administrativa

--- dscommerce-frontend-login-crud/src/App.tsx
+++ dscommerce-frontend-login-crud/src/App.tsx
@@ -16,6 +16,8 @@ import { ContextCartCount } from './utils/context-cart';
 import { PrivateRoute } from './components/PrivateRoute';
 import type { AccessTokenPayloadDTO } from './models/auth';
 import { ContextToken } from './utils/context-token';
+import ProductListing from './routes/Admin/ProductListing';
+import ProductForm from './routes/Admin/ProductForm';
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(
@@ -48,24 +50,13 @@ export default function App() {
               />
               <Route path="cart" element={<Cart />} />
               <Route path="login" element={<Login />} />
-              <Route
-                path="confirmation/:orderId"
-                element={
-                  <PrivateRoute>
-                    <Confirmation />
-                  </PrivateRoute>
-                }
-              />
+              <Route path="confirmation/:orderId" element={<PrivateRoute><Confirmation /></PrivateRoute>}/>
             </Route>
-            <Route
-              path="/admin/"
-              element={
-                <PrivateRoute roles={['ROLE_ADMIN']}>
-                  <Admin />
-                </PrivateRoute>
-              }
-            >
-              <Route index element={<AdminHome />} />
+            <Route path="/admin/" element={<PrivateRoute roles={['ROLE_ADMIN']}><Admin /></PrivateRoute>}>
+              <Route index element={<Navigate to="/admin/home"/> } />
+              <Route path="home" index element={<AdminHome />} />
+              <Route path="products" element={<ProductListing />} />
+              <Route path="products/:productId" element={<ProductForm />} />
             </Route>
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
