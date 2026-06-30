e2f1f8ad36ff9bf0306804c17beccbc2fe664653
Guilherme <guilherme.marrafon1@gmail.com>
2026-06-30 08:01:52 -0300

    Função toDirty do módulo forms

--- dscommerce-frontend-login-crud/src/routes/Admin/ProductForm/index.tsx
+++ dscommerce-frontend-login-crud/src/routes/Admin/ProductForm/index.tsx
@@ -41,6 +41,9 @@ export default function ProductForm() {
   });
 
   useEffect(() => {
+    const result = forms.toDirty(formData, 'price');
+    console.log(result);
+
     if (isEditing) {
       productService.findById(Number(params.productId)).then((response) => {
         const newFormData = forms.updateAll(formData, response.data);
@@ -59,6 +62,11 @@ export default function ProductForm() {
     setFormData(dataValidated);
   }
 
+  function handleTurnDirty(name: string) {
+    const newFormData = forms.toDirty(formData, name);
+    setFormData(newFormData);
+  }
+
   return (
     <main>
       <section id="product-form-section" className="dsc-container">
@@ -70,6 +78,7 @@ export default function ProductForm() {
                 <FormInput
                   {...formData.name}
                   className="dsc-form-control"
+                  onTurnDirty={handleTurnDirty}
                   onChange={handleInputChange}
                 />
                 <div className="dsc-form-error">{formData.name.message}</div>
@@ -78,6 +87,7 @@ export default function ProductForm() {
                 <FormInput
                   {...formData.price}
                   className="dsc-form-control"
+                  onTurnDirty={handleTurnDirty}
                   onChange={handleInputChange}
                 />
                 <div className="dsc-form-error">{formData.price.message}</div>
@@ -86,6 +96,7 @@ export default function ProductForm() {
                 <FormInput
                   {...formData.imgUrl}
                   className="dsc-form-control"
+                  onTurnDirty={handleTurnDirty}
                   onChange={handleInputChange}
                 />
               </div>
